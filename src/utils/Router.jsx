a9fcef93bc853f0cb4191bf6a4cc8056c0b35f5d
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-04-08 22:22:46 +0530

    Updated Routing

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -1,12 +1,8 @@
 export { Header } from "../components/Header";
 export { Hero } from "../screen/Hero";
-export { Service } from "../screen/Service";
-export { Portfolio } from "../screen/portfolio/Portfolio";
-export { PortfolioDetails } from "../screen/portfolio/PortfolioDetails";
+export { Portfolio } from "../screen/Portfolio";
 export { Resume } from "../screen/Resume";
 export { Skill } from "../screen/Skill";
-export { Testimonial } from "../screen/Testimonial";
-export { Blog } from "../screen/Blog";
 export { Contact } from "../screen/Contact";
 export { Footer } from "../components/Footer";
 export { Layout } from "../components/Layout";
